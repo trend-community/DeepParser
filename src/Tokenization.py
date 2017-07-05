3c42265c7683a29e234e813e7944e3bf386dd519
ben <tianbing.lin@jd.com>
2017-07-05 15:38:57 -0700

    add ontology

---
+++ src/Tokenization.py
@@ -0,0 +1,71 @@
+#!/bin/python
+#read a file in main(), then do tokenization.
+import logging
+import nltk     #TODO: remove this package in the future.
+
+class EmptyBase(object): pass
+DS = []    # this present the whole data structure.
+
+def Tokenize(sentence):
+    global DS
+    tokens = nltk.word_tokenize(sentence)
+
+    for token in tokens:
+        Element = EmptyBase()
+        Element.word = token
+        Element.position = 1
+        DS.append(Element)
+
+
+
+def Tokenize_mine(sentence):
+    global DS
+
+    StartToken = True
+    StartPosition = 0
+    #for i in range(1, len(sentence)):   #ignore the first one.
+    i = 1
+    while i<len(sentence):
+        c = sentence[i]
+        prevc = sentence[i-1]
+        if c == "'":
+            if 0<i<len(sentence)-1 and sentence[i-1].isalpha() and sentence[i+1].isalpha():
+                i+=2
+                continue    #when ' sign is inside a word, like can't don't
+
+        if (prevc.isalnum() and not c.isalnum()) or (not prevc.isalnum() and not prevc.isspace()):
+            Element = EmptyBase()
+            Element.word = sentence[StartPosition:i]
+            Element.position = StartPosition
+            DS.append(Element)
+            StartToken = False
+
+        if (c.isalnum() and (not prevc.isalnum()) ) or (not c.isalnum() and not c.isspace()):
+            StartToken = True
+            StartPosition = i;
+        i += 1
+
+    if StartToken:  #wrap up the last one
+        Element = EmptyBase()
+        Element.word = sentence[StartPosition:]
+        Element.position = StartPosition
+        DS.append(Element)
+
+
+def DisplayDS():
+    for ds in DS:
+        print "[word]:" + ds.word + "\t[position]:" + str(ds.position)
+
+
+if __name__ == "__main__":
+    logging.basicConfig( level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+    target = "This is a 'bad_sentence', not a word. Don't classify it as a character."
+    print target
+    Tokenize(target)
+    print "\n\n NLTK tokenization:"
+    DisplayDS()
+
+    print "\n\n Mine tokenization:"
+    DS = []
+    Tokenize_mine(target)
+    DisplayDS()
