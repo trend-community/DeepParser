cd2708105a4f27e0a89d42206e6c1d6ffbbb328d
xiaochenhou <xh5@illinois.edu>
2017-08-09 11:11:01 -0700

    replace ancestor

--- src/FeatureOntology.py
+++ src/FeatureOntology.py
@@ -127,6 +127,7 @@ class OntologyNode:
         #Now the last block has the real feature name and code
         code = blocks[-1]
         realfeature = blocks[0]
+        realfeatureId = GetFeatureID(realfeature)
 
         featureID = GetFeatureID(realfeature)
         if featureID == -1: #the feature in file is not in featureFullList
@@ -144,6 +145,7 @@ class OntologyNode:
         lastalias = features[0]
         for alias in blocks[1:-1] + [lastalias]:
             aliasnode = SearchFeatureOntology(GetFeatureID(alias))
+            aliasId = GetFeatureID(alias)
             if aliasnode:
                 realnode.ancestors.update(aliasnode.ancestors)
                 aliasnode.ancestors.clear()
@@ -151,6 +153,12 @@ class OntologyNode:
                 #   replace that as the realfeature (featureID)
                 if alias in _FeatureSet:
                     _FeatureSet.remove(alias)
+
+            for node in _FeatureOntology:
+                if aliasId in node.ancestors:
+                    node.ancestors.remove(aliasId)
+                    node.ancestors.add(realfeatureId)
+
             _AliasDict[alias] = featureID
 
 
@@ -207,7 +215,7 @@ def PrintFeatureOntology():
             print(node)
     print("//***Alias***")
     for key in sorted(_AliasDict):
-        print( key + "=" + _FeatureList[_AliasDict[key]])
+        print( _FeatureList[_AliasDict[key]] + "=" + key )
 
 def PrintLexicon(flag):
     print("//***Lexicon***")
@@ -373,7 +381,7 @@ if __name__ == "__main__":
         LoadFeatureOntology(dir_path + '/../../fsa/Y/feature.txt')
         para = dir_path + '/../../fsa/Y/lexY.txt'
         LoadLexicon(para)
-        if "lexX" in para:
+        if "LexX" in para:
             flag = False
         else:
             flag = True
