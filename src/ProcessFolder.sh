b005227cce02c34954a0a331427ce8c2c28b8070
ben <tianbing.lin@jd.com>
2017-10-10 16:19:08 -0700

    process multiple files at the same time, to utilize multiple cpu

--- src/ProcessFolder.sh
+++ src/ProcessFolder.sh
@@ -8,6 +8,6 @@ do
     echo "Processing $f file..."
     filename=$(basename "$f")
     outputfile="$OUTPUTFOLDER/$filename"
-    python MultiLevelSegment.py "$f" NoFeature > "$outputfile" 2>> ../log/ProcessFolder.log
-    python MultiLevelSegment.py "$f" Debug > "$outputfile_feature.txt" 2>> ../log/ProcessFolder.log
+    python MultiLevelSegment.py "$f" NoFeature > "$outputfile" 2>> ../log/ProcessFolder.log &
+    python MultiLevelSegment.py "$f" Debug > "$outputfile_feature.txt" 2>> ../log/ProcessFolder.log &
 done
