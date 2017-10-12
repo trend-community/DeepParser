89786d28c474bc3b1f57a581b5a44318a8087683
ben <tianbing.lin@jd.com>
2017-10-12 08:49:28 -0700

    try the speed of each

--- src/ProcessFolder.sh
+++ src/ProcessFolder.sh
@@ -3,12 +3,21 @@
 INPUTFILES=../../fsa/test/input/*
 OUTPUTFOLDER=../../fsa/test/output
 
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+OUTPUTFOLDER=$OUTPUTFOLDER/$current_time
+
 for f in $INPUTFILES
 do
     echo "Processing $f file..."
     filename=$(basename "$f")
     outputfile="$OUTPUTFOLDER/$filename"
     python MultiLevelSegment.pyc "$f" NoFeature > "$outputfile" 2>> ../log/ProcessFolder.log &
-    python MultiLevelSegment_RestfulService.pyc "$f" Debug > "$outputfile_feature.txt" 2>> ../log/ProcessFolder_feature.log
 done
 
+for f in $INPUTFILES
+do
+    echo "Processing $f file..."
+    filename=$(basename "$f")
+    outputfile="$OUTPUTFOLDER/$filename"
+    python MultiLevelSegment_RestfulService.pyc "$f" Debug > "$outputfile_feature.txt" 2>> ../log/ProcessFolder_feature.log &
+done
