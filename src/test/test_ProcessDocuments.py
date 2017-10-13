7ef6f2d1d068db81e6b405deecad117ba5426dbe
ben <tianbing.lin@jd.com>
2017-10-13 15:02:37 -0700

    process documents

---
+++ src/test/test_ProcessDocuments.py
@@ -0,0 +1,13 @@
+
+import unittest
+from ProcessDocuments import *
+
+
+class RuleTest(unittest.TestCase):
+    def test_SentenceSegmentation(self):
+        ss = SentenceSegmentation("味道还是不错。支持博达，下次还来买。")
+        self.assertEqual(len(ss), 2)
+
+    def test_MultiSentenceSegmentation(self):
+        ss = SentenceSegmentation("味道还是不错。支持博达；下次还来买。")
+        self.assertEqual(len(ss), 3)
