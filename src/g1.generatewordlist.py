de9b1851e2d508546d86759a3610c916c9425d52
ben <tianbing.lin@jd.com>
2017-10-27 17:06:49 -0700

    generate word list

--- src/g1.generatewordlist.py
+++ src/g1.generatewordlist.py
@@ -4,7 +4,7 @@
 #==============================================================
 import argparse
 import pickle
-from viterbi1 import *
+import viterbi1
 
 parser = argparse.ArgumentParser()
 parser.add_argument("output", help="output folder")
@@ -12,17 +12,16 @@ parser.add_argument("dict", help="pickle dict")
 args = parser.parse_args()
 print(args)
 
-LoadDictFromPickle(args.dict)
+viterbi1.LoadDictFromPickle(args.dict)
 #==============================================================
 # unigram tokenization
 #==============================================================
 import codecs, re
 
 for i in range(1, 15):
-
     fout = codecs.open(args.output+"/gran_" + str(i) + "_list.txt", 'wb', encoding='utf-8')
 
-    shorlist = [k for k in querydict if len(k) == i]
+    shorlist = [k for k in viterbi1.querydict if len(k) == i]
     for word in sorted(shorlist):
         word = word.replace(":", "\:")
         fout.write(word + "\n")
