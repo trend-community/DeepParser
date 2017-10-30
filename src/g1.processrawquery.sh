7df3effbfba1b5923f105d1d454ed06688ad4466
ben <tianbing.lin@jd.com>
2017-10-30 14:08:11 -0700

    remove control character except 0A 0D 01

--- src/g1.processrawquery.sh
+++ src/g1.processrawquery.sh
@@ -17,7 +17,7 @@
 #Usage:
 #bash g1.processrawquery.sh rawfilelocation dictfilelocation rulefilefolder, lexiconfilefolder
 
-sed -e "s/\|\|\|//g" $1 > /tmp/raw_wo_ctrl.txt
+sed -e "s/[\x02-\x09\x0b-\x0c\x0e-\x1a]//g" $1 > /tmp/raw_wo_ctrl.txt
 python g1.norm.py /tmp/raw_wo_ctrl.txt /tmp/dictoutput.txt $2
 
 mkdir -p $3
