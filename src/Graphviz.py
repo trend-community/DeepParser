21323a953d58c4755881ffe555e1c815c714f2ab
ben <tianbing.lin@jd.com>
2017-11-11 14:15:22 -0800

    write image part

--- src/Graphviz.py
+++ src/Graphviz.py
@@ -3,6 +3,7 @@ import pydot
 import json
 import time
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 nodeList = []
 class Node(object):
@@ -76,13 +77,12 @@ def showGraph(json_input):
     print("size of list is " + str(len(nodeList)))
     # printTree(nodeList)
     OrgGraph()
-    filename = dir_path + '/../../parser/graph/' + time.strftime("%Y%m%d-%H%M%S")+'.svg'
+    filename = os.path.join(dir_path, '../../parser/graph/' ,  time.strftime("%Y%m%d-%H%M%S")+'.svg')
     # graph.write_svg('g1.svg')
     graph.write_svg(filename)
 
 
 if __name__ == "__main__":
-    dir_path = os.path.dirname(os.path.realpath(__file__))
     json_input = '{"EndOffset": 8, "StartOffset": 0, "features": [], "sons": [{"EndOffset": 4, "StartOffset": 0, "features": ["v", "equivN", "chg", "deverbal", "V0", "v2NN", "exercise", "N", "act", "chgLoc", "entice", "attrC"], "sons": [{"EndOffset": 2, "StartOffset": 0, "UpperRelationship": "^.M", "features": ["v", "0", "N", "advV"], "text": "满减"}, {"EndOffset": 4, "StartOffset": 2, "features": ["v", "equivN", "chg", "deverbal", "V0", "v2NN", "0", "NP", "XP", "exercise", "N", "act", "chgLoc", "entice", "attrC"], "text": "活动"}], "text": "满减活动"}, {"EndOffset": 8, "StartOffset": 4, "features": ["sent", "pt", "Pred", "A", "pred", "pro"], "sons": [{"EndOffset": 6, "StartOffset": 4, "UpperRelationship": "^.R", "features": ["0", "pt", "emph", "ptA", "A", "pEmo", "attitude", "sent", "a", "an", "passion", "property", "rank", "intensifier", "good", "daxingC"], "text": "超级"}, {"EndOffset": 8, "StartOffset": 6, "features": ["0", "pt", "A", "AP", "pred", "pro", "sent", "XP", "Pred"], "text": "划算"}], "text": "超级划算"}], "text": "满减活动超级划算"}'
     showGraph(json_input)
 
