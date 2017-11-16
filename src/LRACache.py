3a10af62210267ef69db1a328f108b550cc9491f
ben <tianbing.lin@jd.com>
2017-11-15 22:04:05 -0800

    todo

--- src/LRACache.py
+++ src/LRACache.py
@@ -1,17 +1,26 @@
 import collections
 
+#Reference: https://docs.python.org/3/reference/datamodel.html?emulating-container-types#emulating-container-types
+# https://stackoverflow.com/questions/3387691/how-to-perfectly-override-a-dict
+
 class LRACache(collections.MutableMapping):
     """A dictionary that applies an arbitrary key-altering
        function before accessing the keys"""
+    def setlimit(self, limit):
+        self.limit = limit
 
     def __init__(self, *args, **kwargs):
         self.store = dict()
         self.update(dict(*args, **kwargs))  # use the free update to set keys
+        self.limit = 0
 
     def __getitem__(self, key):
+        #//TODO: add weight to this key, or put it to the latest order.
         return self.store[key]
 
     def __setitem__(self, key, value):
+        if 0 <  self.limit <= len(self.store)  :
+            pass    #//TODO: delete last not used limit
         self.store[key] = value
 
     def __delitem__(self, key):
