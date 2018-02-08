856eb0df6e40328809cbbfe9b3c9a167bbd4edb6
Ben Lin <tianbing.lin@jd.com>
2018-02-08 10:42:12 -0800

    style change

--- src/ProcessSentence.py
+++ src/ProcessSentence.py
@@ -199,7 +199,7 @@ def MatchAndApplyRuleFile(strtokenlist, RuleFileName):
                 if WinningRule.RuleName not in WinningRules:
                     WinningRules[WinningRule.RuleName] = '<li>' + WinningRule.Origin + ' <li class="indent">' + MarkWinningTokens(strtokenlist, WinningRule, i)
                 else:
-                    WinningRules[WinningRule.RuleName] += " " + MarkWinningTokens(strtokenlist, WinningRule, i)
+                    WinningRules[WinningRule.RuleName] += ' <li class="indent">' + MarkWinningTokens(strtokenlist, WinningRule, i)
                 ApplyWinningRule(strtokenlist, WinningRule, StartPosition=i)
                 #logging.debug("After applied: " + jsonpickle.dumps(strtokenlist))
             except RuntimeError as e:
