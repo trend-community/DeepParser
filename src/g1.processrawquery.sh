99b92ab8834836b4a827fc96ad24c2eaaa1878f8
Ben Lin <tianbing.lin@jd.com>
2018-05-02 15:07:11 -0700

    folder re-arrange

--- src/g1.processrawquery.sh
+++ src/g1.processrawquery.sh
@@ -20,36 +20,36 @@
 
 set -x
 
-mkdir -p $2/temp
-mkdir -p $2/lexicon
-mkdir -p $2/rule
+mkdir -p $3/temp
+mkdir -p $3/lexicon
+mkdir -p $3/rule
 
 #perl -pi -e 's/\x01/\t/g' $1
 
-LC_ALL=C grep -Pva "\t[1-9]$" $1  > $2/temp/raw.10plus.txt
-#LC_ALL=C grep -Pv "\t1[0-9]$" $2/temp/raw.10plus.txt  > $2/temp/raw.100plus.txt  # that removed some useful words. might be able to add up to more than 100. sample: 格纹
-sed -e "s/[[:punct:]]/ /g" $2/temp/raw.10plus.txt > $2/temp/raw.10plus.nopunct.txt    #has to be executed manually to get the Chinese punct correct.
-LC_ALL=C sed -e "s/[\x00-\x08\x0b-\x0c\x0e-\x1a]//g"  $2/temp/raw.10plus.nopunct.txt  > $2/temp/raw_wo_ctrl.10plus.nopunct.txt        #only keep \t \r \n
+LC_ALL=C grep -Pva "\t[1-9]$" $1  > $3/temp/raw.10plus.txt
+#LC_ALL=C grep -Pv "\t1[0-9]$" $3/temp/raw.10plus.txt  > $3/temp/raw.100plus.txt  # that removed some useful words. might be able to add up to more than 100. sample: 格纹
+sed -e "s/[[:punct:]]/ /g" $3/temp/raw.10plus.txt > $3/temp/raw.10plus.nopunct.txt    #has to be executed manually to get the Chinese punct correct.
+LC_ALL=C sed -e "s/[\x00-\x08\x0b-\x0c\x0e-\x1a]//g"  $3/temp/raw.10plus.nopunct.txt  > $3/temp/raw_wo_ctrl.10plus.nopunct.txt        #only keep \t \r \n
 
-python3 g1.norm.py $2/temp/raw_wo_ctrl.10plus.nopunct.txt ../../fsa/X/LexBlacklist.txt ../../fsa/X/LexBlacklist_TopChars.txt $2/temp/dictoutput.txt $2 2>../temp/g1.norm.error.txt
+python3 g1.norm.py $3/temp/raw_wo_ctrl.10plus.nopunct.txt ../../fsa/X/LexBlacklist.txt ../../fsa/X/LexBlacklist_TopChars.txt $3/temp/dictoutput.txt $2 2>../temp/g1.norm.error.txt
 
-python3 g1.generatewordlist.py $2/temp $2     2>../temp/g1.generatewordlist.error.txt
+python3 g1.generatewordlist.py $3/temp $2     2>../temp/g1.generatewordlist.error.txt
 
 
-cat $3/AllLexicon.txt | awk -F: '{print $1}' > $2/temp/SystemLexicon.txt
-cat $3/defLexX.txt | awk -F: '{print $1}' > $2/temp/SystemLexicon_defLex.txt
-cat $3/defPlus.txt | awk -F: '{print $1}' >> $2/temp/SystemLexicon_defLex.txt
-for f in $2/temp/gram*
+cat $4/AllLexicon.txt | awk -F: '{print $1}' > $3/temp/SystemLexicon.txt
+cat $4/defLexX.txt | awk -F: '{print $1}' > $3/temp/SystemLexicon_defLex.txt
+cat $4/defPlus.txt | awk -F: '{print $1}' >> $3/temp/SystemLexicon_defLex.txt
+for f in $3/temp/gram*
 do
     echo "processing $f ..."
     filename=$(basename "$f")
-    outputfile="$2/temp/Mixed_$filename"
-    python3 g1.sent.py  "$f" "$outputfile" $2 $2/temp/SystemLexicon.txt $2/temp/SystemLexicon_defLex.txt
+    outputfile="$3/temp/Mixed_$filename"
+    python3 g1.sent.py  "$f" "$outputfile" $3 $3/temp/SystemLexicon.txt $3/temp/SystemLexicon_defLex.txt
 
-    newlexiconname="$2/lexicon/CleanLexicon_$filename"
-    grep -va "<" $outputfile | grep -Fxv -f $2/temp/SystemLexicon.txt > $newlexiconname &
+    newlexiconname="$3/lexicon/CleanLexicon_$filename"
+    grep -va "<" $outputfile | grep -Fxv -f $3/temp/SystemLexicon.txt > $newlexiconname &
 
-    newrulename="$2/rule/CleanRule_$filename_ngram"
+    newrulename="$3/rule/CleanRule_$filename_ngram"
     echo "CleanRule_$filename ==  // $filename \n" > $newrulename
     # 1, search for rule; 2, add quotes for english word, 3, add rule name for each 2000 lines. | awk '1;!(NR%2000){print "CleanRule_$filename ==";}'
     grep -a "<" $outputfile | sed  -E  's/([< ])([a-z0-9A-Z][a-z0-9A-Z]*)([ >])/\1\[\x27\2\x27\]\3/g'   >> $newrulename &
