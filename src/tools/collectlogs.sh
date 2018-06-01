0e7539106504e5a4114467335c3b9504dfcd1345
Ben Lin <tianbing.lin@jd.com>
2018-06-01 10:23:14 -0700

    collect logs.

--- src/tools/collectlogs.sh
+++ src/tools/collectlogs.sh
@@ -3,15 +3,15 @@ mkdir 135
 rsync root@172.18.189.135:/export/App/git/parser/log/*.log 135/
 
 
-for f in $(find . -maxdepth 1 -name *.log)
+for f in $(find .   -name *.log)
 do
-  awk '{F=substr($0,1,10)".txt";print >> F;close(F)}' $f
+  grep COMPLETE $f |grep -v "ab cd" | awk '{F=substr($0,1,10)".txt";print >> F;close(F)}'
 done
 
 # sort and de-duplicate
 # remove heart beat
 for f in *.txt
 do
-  sort -u $f | grep -v "ab cd" | > $f.sorted.txt
+  sort -u $f |  > $f.sorted.txt
   cut -d' ' -f 2 $f.sorted.txt | cut -d':' -f 1-2  > $f.time.txt
 done
