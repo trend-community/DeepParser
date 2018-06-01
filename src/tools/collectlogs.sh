a107e9bb016abe5a7b3cf14875b3e1f36e184a33
Ben Lin <tianbing.lin@jd.com>
2018-06-01 13:24:09 -0700

    collect logs.

--- src/tools/collectlogs.sh
+++ src/tools/collectlogs.sh
@@ -5,13 +5,13 @@ rsync root@172.18.189.135:/export/App/git/parser/log/*.log 135/
 
 for f in $(find .   -name *.log)
 do
-  grep COMPLETE $f |grep -v "ab cd" | awk '{F=substr($0,1,10)".txt";print >> F;close(F)}'
+  grep COMPLETE $f |grep -v "ab cd" | awk '{F=substr($0,1,10)".log";print >> F;close(F)}'
 done
 
 # sort and de-duplicate
 # remove heart beat
-for f in *.txt
+for f in *.log
 do
-  sort -u $f |  > $f.sorted.txt
+  sort -u $f   > $f.sorted.txt
   cut -d' ' -f 2 $f.sorted.txt | cut -d':' -f 1-2  > $f.time.txt
 done
