a2729ecd94e7a994ff8f010e8813fa057863ad96
Ben Lin <tianbing.lin@jd.com>
2018-11-02 11:31:23 -0700

    good nginx.conf. failed to separate.

--- src/tools/import.sales.log.py
+++ src/tools/import.sales.log.py
@@ -1,115 +1,130 @@
-
+#Log info: https://cf.jd.com/pages/viewpage.action?pageId=138728223
 import  sys, logging
-import sqlite3, jsonpickle, json, re
+import sqlite3, jsonpickle, json
+"""
+#!/bin/sh
+#getlog.sh
+set -x
 
+hive -e "select * from fdm.fdm_hbase_crius_c_alpha_sales_log_alpha_sales_event_tracking_log where dt='$1'" > alllog.$1.log
+hive -e "select * from fdm.fdm_hbase_crius_c_alpha_sales_answer_alpha_sales_info_log where dt='$1'" > extrainfo.$1.log
+
+python3 import.sales.log.py alllog.$1.log extrainfo.$1.log log.$1.db
+
+
+"""
 def PreProcess():
     try:
-        cur.execute("""CREATE TABLE alllog ( rowkey text, request_id text, timestamp text, customer_pin text, staff_pin text, plugin_state text,
-            recommend_state text, adopt_state text, customer_question text, ai_answers text,
-            adopted_answer text, staff_response text, session_id text, qa_pair_id text, 
-            position text, additional_info text, dt text, sku text);""")
+        cur.execute("""CREATE TABLE alllog ( pair_id text, pair_id2 text, sid text, ts text, staff text, customer text, 
+            question text, adopted text, response text, r_state text, a_state text,
+            position text, plugin_state text, sku_id text, brand text, cate text, 
+            shop_id text, intent_1 text, intent_2 text, confidence text, source text,
+            is_presale text, answer_top_1 text, test_flag text, log_type text, create_date text,
+            dt text);""")
     except sqlite3.OperationalError:
         logging.warning("Table existed!")
     try:
-        cur.execute("""CREATE TABLE answer ( rowkey text, sequenceid int, answer text, optional text,
-            pairId text, question text, score float, sourceList text);""")
+        cur.execute("""CREATE TABLE answer ( pair_id text, pair_id2 text,sequenceid int, sid int, question text, answer text, sourcelist text,
+             score float, confidencescore float, optional text, xgboostscore float, sku text,
+             ai_question text, additional_info text, create_date text, dt text);""")
     except sqlite3.OperationalError:
         logging.warning("Table existed!")
 
 
     try:
-        cur.execute("""CREATE index loganswer_i on alllog (customer_question, adopted_answer, rowkey);""")
-        cur.execute("""CREATE index session_i on alllog (session_id, timestamp, rowkey);""")
+        cur.execute("""CREATE index loganswer_i on alllog (question, adopted, sid);""")
+        cur.execute("""CREATE index session_i on alllog (sid, ts, pair_id);""")
+        cur.execute("""CREATE unique index pairid on alllog(pair_id, ts, question)""")
 
-        cur.execute("""create index a_i on answer (answer, question, rowkey, sequenceid);""")
-        cur.execute("""create index aq_i on answer (question, answer, score, rowkey, sequenceid);""")
-        cur.execute("""create index a_id on answer (rowkey, sequenceid, answer, question);""")
+        cur.execute("""CREATE unique index a_pairid on answer(pair_id, sequenceid, question) """)
+        cur.execute("""create index a_i on answer (answer, question, pair_id, sid);""")
+        cur.execute("""create index aq_i on answer (question, answer, score, pair_id, sid);""")
+        cur.execute("""create index a_id on answer (pair_id, sid, answer, question);""")
     except sqlite3.OperationalError:
         logging.warning("Index existed!")
 
 
-def StoreAIAnswers(rowkey, text):
-    if len(text) < 6:
+def StoreAIAnswers(row):
+
+    if len(row[4]) < 6:
         return
-    text = text[3:-3].replace("   \",", "\",").replace("   \"}", "\"}").replace("   \"}", "\"}").replace("  \"}",
+    answertext = row[4][3:-3].replace("   \",", "\",").replace("   \"}", "\"}").replace("   \"}", "\"}").replace("  \"}",
                     "\"}").replace("   \"", "``").replace("哦\",</i>", "哦`,</i>").replace("哦\", \",", "哦`, \",")
     #remove three space+quotes, if there is no comma following.
-    ans = text.split("}\",\"{")
+    ans = answertext.split("}\",\"{")
+
+    InsertAIQuery = "insert or ignore into answer values("+ ",".join(["?" for _ in range(16)]) + ")"
     sequenceid = 0
-    InsertAIQuery = "insert into answer values("+ ",".join(["?" for _ in range(8)]) + ")"
     for an in ans:
         answer_text = "{" + an + "}"
         try:
             answer = jsonpickle.decode(answer_text)
         except json.decoder.JSONDecodeError as e:
-            logging.warning("this answer_text failed to decode: {} in rowkey:{}\nReason:{}".format(answer_text, rowkey, e))
+            logging.warning("this answer_text failed to decode: {} in row:{}\nReason:{}".format(answer_text, str(row), e))
             continue
         #print(answer)
-        cur.execute(InsertAIQuery, [rowkey, sequenceid, answer['answer '].strip(), str(answer['optional ']),
-                    answer['pairId '].strip() if 'pairId ' in answer else '', answer['question '].strip(),
-                    answer['score '], str(answer['sourceList '])
-                                  ])
-        sequenceid += 1
-
-
-def StoreAIAnswers_new(rowkey, text):
-    sequenceid = 0
-    InsertAIQuery = "insert into answer values("+ ",".join(["?" for _ in range(8)]) + ")"
-    json_array = json.loads(text)
-    #answerlist = jsonpickle.decode(text)
-    for answer_text in json_array:
         try:
-            answer = jsonpickle.decode(answer_text)
-        except json.decoder.JSONDecodeError:
-            logging.warning("this answer_text failed to decode: {} in \n{}".format(answer_text, text))
-            continue
-        #print(answer)
-        cur.execute(InsertAIQuery, [rowkey, sequenceid, answer['answer '].strip(), str(answer['optional ']),
-                    answer['pairId '].strip() if 'pairId ' in answer else '', answer['question '].strip(),
-                    answer['score '], str(answer['sourceList '])
+            cur.execute(InsertAIQuery, [row[0], row[1], sequenceid, row[2], row[3], answer['answer '].strip(), str(answer['sourceList ']),
+                    answer['score '], answer['confidenceScore '], str(answer['optional ']),
+                    answer['xgboostScore '], answer['sku '].strip() if 'sku ' in answer else '',
+                    answer['question '], row[5], row[6], row[7]
                                   ])
+        except (RuntimeError,KeyError) as e:
+            logging.error(e)
+            logging.error("\tanswer_text: " + answer_text)
+            logging.error("\tline:" + str(row))
         sequenceid += 1
 
 
 #log is created from hive -e "select * from fdm.fdm_hbase_crius_c_alpha_sales_alpha_sales_log" > log.txt
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3 :
-        print("Usage: python3 import.sales.log.py [inputfile] [outputdb]  ")
+    if len(sys.argv) != 4 :
+        print("Usage: python3 import.sales.log.py [inputfile_log] [inputfile_aianswer] [outputdb]  ")
         exit(1)
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
     #outfile = open(sys.argv[2], 'w', encoding="utf-8")
-    DBCon = sqlite3.connect(sys.argv[2])
+    DBCon = sqlite3.connect(sys.argv[3])
     cur = DBCon.cursor()
     PreProcess()
-    InsertQuery="insert into alllog values(" + ",".join(["?" for _ in range(18)]) + ")" #including sku
+    InsertQuery="insert or ignore into alllog values(" + ",".join(["?" for _ in range(27)]) + ")" #including sku
     print("InsertQuery:\t{}".format(InsertQuery))
     #ai_answerid = 0
     with open(sys.argv[1], 'r', encoding="utf-8") as RuleFile:
-
         for line in RuleFile:
             columns = line.split("\t")
             columns = [x.strip() for x in columns]
-            if len(columns) != 17:
+            if len(columns) != 27:
                 logging.warning("this line has different tab:{}\n\t{}".format(len(columns), line))
                 continue
-            if columns[9] != "[]" and len(columns[9]) > 4:
-                StoreAIAnswers(columns[0], columns[9])
-                columns[9] = ''
-
-            columns.append("")  #sku
-            if columns[15] != "{}":  #additional info, contains sku
-                addition_info = jsonpickle.decode(columns[15])
-                if "sku" in addition_info:
-                    columns[17] = addition_info["sku"]
+            # if columns[9] != "[]" and len(columns[9]) > 4:
+            #     StoreAIAnswers(columns[0], columns[9])
+            #     columns[9] = ''
+            #
+            # columns.append("")  #sku
+            # if columns[15] != "{}":  #additional info, contains sku
+            #     addition_info = jsonpickle.decode(columns[15])
+            #     if "sku" in addition_info:
+            #         columns[17] = addition_info["sku"]
 
             cur.execute(InsertQuery, columns)
         DBCon.commit()
 
-    cur.execute("delete from answer where rowkey in (select rowkey from alllog where staff_pin like '%test%');")
-    cur.execute("delete from alllog where staff_pin like '%test%';")
+    logging.info("Done with {}. Start importing from {}".format(sys.argv[1], sys.argv[2]))
+    with open(sys.argv[2], 'r', encoding="utf-8") as RuleFile:
+        for line in RuleFile:
+            columns = line.split("\t")
+            columns = [x.strip() for x in columns]
+            if len(columns) != 8:
+                logging.warning("this line has different tab:{}\n\t{}".format(len(columns), line))
+                continue
+            StoreAIAnswers(columns)
+        DBCon.commit()
+
+#    cur.execute("delete from answer where rowkey in (select rowkey from alllog where staff_pin like '%test%');")
+#    cur.execute("delete from alllog where staff_pin like '%test%';")
     cur.close()
     DBCon.commit()
     DBCon.close()
--- src/tools/qa_importlog.py
+++ src/tools/qa_importlog.py
@@ -6,7 +6,7 @@ import sqlite3, re
 def InsertSession(OneSession):
     if not hasattr(InsertSession, "SessionID"):
         InsertSession.SessionID = 0
-    InsertQuery = "insert into qalog (sessionid, timestamp, sku, userid, sentence) values (?, ?, ?, ?, ?)"
+    InsertQuery = "insert into qalog (sessionid, timestamp, sku, userid, sentence, sentence_n) values (?, ?, ?, ?, ?, ?)"
     sku = ""
     InsertSession.SessionID += 1
     for oneline in OneSession.split("\n"):
@@ -16,67 +16,224 @@ def InsertSession(OneSession):
         QAmatch = re.match("(\d*)\t(.*?):(.*)", oneline)
         if QAmatch:
             sentence = QAmatch.group(3).strip()
-            if not sku:
-                skumatch = re.match(" http://item\.jd\.com/(\d*)\.html", sentence)
-                if skumatch:
-                    sku = skumatch.group(1).strip()
-            if not sku:
-                skumatch = re.search("商品编号：(\d*)", sentence)
-                if skumatch:
-                    sku = skumatch.group(1).strip()
+            skumatch = re.match(" http://item\.jd\.com/(\d*)\.html", sentence)
+            if skumatch:
+                sku = skumatch.group(1).strip()
+            skumatch = re.search("商品编号：(\d*)", sentence)
+            if skumatch:
+                sku = skumatch.group(1).strip()
 
             if  not sku :
                 logging.error("A session without sku: {}".format(OneSession))
-            cur.execute(InsertQuery, [InsertSession.SessionID, int(QAmatch.group(1)), sku, QAmatch.group(2).strip(), sentence])
+            cur.execute(InsertQuery, [InsertSession.SessionID, int(QAmatch.group(1)), sku,
+                                      QAmatch.group(2).strip(), sentence, normalization(sentence)])
 
 
 def PreProcess():
-    cur.execute("create table qalog (ID INTEGER PRIMARY KEY AUTOINCREMENT, sessionid int, timestamp int, sku text, userid text, sentence text, QA int, reference int);")
+    cur.execute("""create table qalog (ID INTEGER PRIMARY KEY AUTOINCREMENT, sessionid int, timestamp int, sku text, 
+        userid text, sentence text, QA int, reference int, sentence_n text);""")
+    cur.execute("""CREATE TABLE qapair(sessionid int, sku text, q text, a text, qid int, aid int)""")
+    cur.execute("""CREATE INDEX a on qapair (a, q);""")
+    cur.execute("""CREATE INDEX q on qapair (q, a);""")
+    cur.execute("""CREATE INDEX aid on qapair (aid, q);""")
+    cur.execute("""CREATE INDEX qid on qapair (qid, a);""")
 
 
-def PostProcess():
-    cur.execute("create index index_sessionid on qalog(sessionid);")
+def PostImportQALogProcess():
+    logging.info("PostImportQALogProcess")
+    cur.execute("create index index_sessionid on qalog(sessionid, QA);")
     cur.execute("create index index_userid on qalog(userid);")
-    cur.execute("create index index_sentence on qalog(sentence);")
+    cur.execute("create index index_sentence on qalog(sentence_n, sentence);")
     cur.execute("create table useridlist as select userid, count(*) as work_count from qalog group by userid;")
     # TODO: use userlist of the sales representatives to set QA=1.
-    # cur.execute("update qalog set QA = 0 ")
-    # cur.execute("update qalog set QA = 1 where userid in (select userid from useridlist where work_count>500);")
-    #
+    cur.execute("update qalog set QA = 0 ")
+    useridlike = sys.argv[3]
+    condition = " or ".join([" userid like \"" + x + "\"" for x in useridlike.split("/")])
+    logging.info("Set Answers: " + "update qalog set QA = 1 where " + condition)
+    cur.execute("update qalog set QA = 1 where " + condition)
+
     # cur.execute("create table sentences (sentenceid integer primary key autoincrement, sentence TEXT, QA INT, sentence_count INT, type int);")
-    # cur.execute(" insert into sentences(sentence, QA, sentence_count, type) select sentence, QA, count(*) as sentence_count, 3 from qalog group by sentence, QA having count(*)>10;")
+    # cur.execute(" insert into sentences(sentence, QA, sentence_count, type) select sentence_n, QA, count(*) as sentence_count, 3 from qalog group by sentence_n, QA having count(*)>1;")
     # cur.execute(" create unique index sentences_s on sentences(sentence, QA, type);")
     #
     # cur.execute(" update sentences set type=1 where sentence like '【%';")
     # cur.execute(" update sentences set type=2 where sentence like '（%';")
-    # # cur.execute("""delete from sentences where sentenceid in (select qalog.sentenceid from sentences as qalog
-    # #                 join sentences on length(qalog.sentence)>length(sentences.sentence) and length(qalog.sentence)<length(sentences.sentence)+5
-    # #                     and instr(qalog.sentence, sentences.sentence)>0
-    # #                     and sentences.type in (1, 2) );""")
-    #
-    # cur.execute("create table closesentence(qalogid int, qasentence text, sentenceid int, type int);")
-    # # cur.execute("""insert into closesentence (qalogid, qasentence, sentenceid, type)
-    # #                 select qalog.id, qalog.sentence, sentences.sentenceid, 0 from qalog
-    # #                 join sentences on qalog.sentence=sentences.sentence and sentences.type in (1, 2);
-    # # """)
-    # cur.execute(""" insert into closesentence (qalogid, qasentence, sentenceid, type)
-    #                 select qalog.id, qalog.sentence, sentences.sentenceid, sentences.type from qalog
-    #                 join sentences on qalog.sentence = sentences.sentence and qalog.QA<>0 and sentences.QA<>0  ;""")
+
+
+def PostQAPairProcess():
+    logging.info("PostQAPairProcess")
+    cur.execute("create table sentences (sentenceid integer primary key autoincrement, sentence TEXT, QA INT, sentence_count INT);")
+    cur.execute(" insert into sentences(sentence, QA, sentence_count) select q, 0, count(*) as sentence_count from qapair group by q having count(*)>1;")
+    cur.execute(" insert into sentences(sentence, QA, sentence_count) select a, 1, count(*) as sentence_count from qapair group by a having count(*)>1;")
+    cur.execute(" create unique index sentences_s on sentences(sentence, QA);")
+    cur.execute(" update qapair set qid=(select sentenceid from sentences where qapair.q=sentences.sentence and QA=0)")
+    cur.execute(" update qapair set aid=(select sentenceid from sentences where qapair.a=sentences.sentence and QA=1)")
+
+
+def ExportQAFiles():
+    logging.info("ExportQAFiles")
+    AQuery = "select sentenceid, sentence, sentence_count from sentences where QA = 1 and sentence_count>=10 order by sentence_count desc "
+
+    cur.execute(AQuery)
+    rows = cur.fetchall()
+
+    Question_cur = DBCon.cursor()
+    QQuery = "select  q from qapair where aid=?"    #not to do "distinct q" in here.
+
+    with open("answerlist.txt", 'w', encoding="utf-8") as answerlist:
+        for row in rows:
+            answerid = row[0]
+            Question_cur.execute(QQuery, [answerid, ])
+            questions = Question_cur.fetchall()
+            if len(questions) < 10:     #too small amount to output/cluster.
+                continue
+
+            answerlist.write("{}\t{}\t{}\n".format(answerid, row[1], row[2]))
+            with open("{}.txt".format(answerid), 'a', encoding="utf-8") as foutput:
+                for question in questions:
+                    foutput.write(question[0]+"\n")
+
+    AQuery = "select sentenceid, sentence, sentence_count from sentences where QA = 0 and sentence_count>=10 order by sentence_count desc "
+    cur.execute(AQuery)
+    rows = cur.fetchall()
+    with open("questionlist.txt", 'w', encoding="utf-8") as questionlist:
+        for row in rows:
+            questionlist.write("{}\t{}\t{}\n".format(row[0], row[1], row[2]))
+
+
+def normalization(inputstr):
+    if not hasattr( normalization, "fulllength"):
+        normalization.fulllength="ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｇｋｌｍ
+ｎｏｐｑｒｓｔｕｖｗｘｙｚ１２３４５６７８９０：-"
+        normalization.halflength="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890:-"
+        normalization.dict_fh = {}
+        for i in range(len(normalization.fulllength)):
+            normalization.dict_fh[normalization.fulllength[i]] = normalization.halflength[i]
+        normalization.signtoremove="！？｡＂＃＄％＆＇（）＊＋，／；＜＝＞＠。［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「
+」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…﹏" \
+                        + "!\"#$%&\'()*+,/;<=>?@[\\]^_`{|}~"
+
+        normalization.stopwordtags={"赞成": "JDSTOPYES", "拒绝":"JDSTOPNO", "无意义":"JDSTOPHELLO", "敏感词": ""}
+        normalization.dict_stopwords = {"敏感词": ["亲", "亲亲", "亲爱的", "宝贝", "宝宝"]}
+        with open(sys.argv[2], encoding="utf-8") as stopwords:
+            for stopword in stopwords:
+                if stopword.startswith("word,"):
+                    continue    #opening line
+                if "," in stopword:
+                    word, tag = stopword.split(",")
+                    tag = tag.strip()
+                    word = word.strip()
+                    if tag in normalization.stopwordtags:
+                        if tag not in normalization.dict_stopwords:
+                            normalization.dict_stopwords[tag] = [word]
+                        else:
+                            normalization.dict_stopwords[tag].append(word)
+
+    temp = re.sub("(https?|ftp)://\S+", " JDHTTP ", inputstr)
+    temp = re.sub("\S+@\S+", " JDHTTP ", temp)
+    temp = re.sub("#E-s\d+", " ", temp)
+    afterfilter = ""
+    for c in temp:
+        if c in normalization.dict_fh:
+            afterfilter += normalization.dict_fh[c]
+            continue
+        if c in normalization.signtoremove:
+            afterfilter += " "
+            continue
+        if '😀' <= c <= '🙏' or c == "☹":
+            afterfilter += " "
+            continue
+        afterfilter += c
+
+    afterreplacestopwords = []
+    for word in afterfilter.split():
+        replaced = False
+        for stopwordtag in normalization.dict_stopwords:
+            if word in normalization.dict_stopwords[stopwordtag]:
+                afterreplacestopwords.append(normalization.stopwordtags[stopwordtag])
+                replaced = True
+                break
+        if not replaced:
+            afterreplacestopwords.append(word)
+
+    return " ".join(afterreplacestopwords)
+
+
+def QAPairSession(s_id):
+    session_cur = DBCon.cursor()
+    SessionQuery = """select sku, sentence_n, QA from qalog where sessionid=? order by timestamp"""
+    session_cur.execute(SessionQuery, [s_id, ])
+    QAWriteQuery = """insert into qapair (sessionid, sku, q, a) values(?, ?, ?, ?)"""
+    rows_s = session_cur.fetchall()
+    q = ""
+    a = ""
+    sku = ""
+    for row_s in rows_s:
+        QA = row_s[2]
+        if QA == 0:
+            if q and a:  # not empty, write the qa into db
+                session_cur.execute(QAWriteQuery, [s_id, sku, q, a])
+                q = ""
+                a = ""
+            if q:
+                q += " " + row_s[1]
+            else:
+                q = row_s[1]
+                sku = row_s[0]  # assume the sku in the first q is the right sku
+
+        else:
+            if a:
+                a += " " + row_s[1]
+            else:
+                a = row_s[1]
+
+    if q and a:  # not empty, write the qa into db
+        session_cur.execute(QAWriteQuery, [s_id, sku, q, a])
+
+    return
+
+
+def GenerateQA():
+    SessionIDQuery = "select distinct sessionid from qalog "
+
+    cur.execute(SessionIDQuery)
+    rows = cur.fetchall()
+    for row in rows:
+        QAPairSession(row[0])
 
 
 if __name__ == "__main__":
+    # x = "this中 文A，B ｃｈｉｎａ。 is , http://abder.dofj.sdf/sjodir/ams in text, this is a@b.c email"
+    # print("before:{}\n   after:{}".format(x, normalization(x)))
+    # x = "a☹︎b😐"
+    # print("before:{}\n   after:{}".format(x, normalization(x)))
+    # x = "亲，这款是二级的不是一级吗 是的 嗯 不能噢 "
+    # print("before:{}\n   after:{}".format(x, normalization(x)))
 
-    if len(sys.argv) < 2:
-        print("Usage: python3 importlog.py [databasefilename] [inputfile1] [inputfile2]...")
+    if len(sys.argv) < 4:
+        print("Usage: python3 importlog.py [databasefilename] [stopwordlist] [staff_pin_match] [inputfile1] [inputfile2]...")
+        print("Example: nohup python3 ../src/qa_importlog.py ../siemens.db ../src/stopwords.csv 博世% ../source/wm*.txt &")
+        print("Example: nohup python3 ../src/qa_importlog.py ../haier.db ../src/stopwords.csv haier%/kaifang% ../source/wm*.txt &")
         exit(1)
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
     DBCon = sqlite3.connect(sys.argv[1])
     cur = DBCon.cursor()
+
+    if sys.argv[2] == "qafile":
+        logging.info("Generate qafile only")
+        ExportQAFiles()
+
+        cur.close()
+        DBCon.commit()
+        DBCon.close()
+        logging.info("Done")
+        exit(0)
+
     PreProcess()
 
-    for filename in sys.argv[2:]:
+    for filename in sys.argv[4:]:
         if not os.path.exists(filename):
             print("Input file " + filename + " does not exist.")
             exit(0)
@@ -97,9 +254,16 @@ if __name__ == "__main__":
 
         DBCon.commit()
 
-    PostProcess()
+    PostImportQALogProcess()
 
+    GenerateQA()
+    PostQAPairProcess()
+    ExportQAFiles()
+
+    logging.info("Start closing DB.")
     cur.close()
     DBCon.commit()
     DBCon.close()
     logging.info("Done")
+
+
